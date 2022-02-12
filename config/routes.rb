cbadf52f9412ee49afee2d054a3735b7554d2563
Fernando Mendes <fernando.msantos1@gmail.com>
2022-02-12 23:15:48 +0000

    Modify layouts, page routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'page/index'
-  get 'page/about'
-  get 'page/contact'
+
+  root 'page#index'
+
+  get 'about', to: 'page#about'
+  get 'contact', to: 'page#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
