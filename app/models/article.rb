623fead5588d637e1ee8752f7dc72fe1fa430b9d
Fernando Mendes <fernando.msantos1@gmail.com>
2022-03-06 02:07:35 +0000

    Validations articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,8 +2,9 @@ class Article < ApplicationRecord
 
   before_save :include_description2
 
-  validates :title, :description, presence: :true
-
+  validates :title, presence: :true, length: { minimum: 3, maximum: 100}
+  validates :description, presence: true, length: {maximum: 200}
+  
   def include_description2
     self.description2 = self.description.capitalize  
   end
