1521365190df3c23d96f61d0abb6102c4748361a
Fernando Mendes <fernando.msantos1@gmail.com>
2022-03-10 00:14:38 +0000

    add campo virtual hash

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,4 +9,29 @@ class Article < ApplicationRecord
     self.description2 = self.description.capitalize  
   end
 
+  def virtual_hash
+    [ 
+      "",
+      [],
+      nil,
+      {},
+      {"0": {}},
+      {"0": {"99": "vazio"} },
+      {"0": {"99": "10"}, "1": {"99": "20"}, "2": {"99": "40"} } 
+    ].sample
+  end
+
+  def self.print_itens
+    all.each do |article|
+      itens = article.virtual_hash # gera uma vez, pois está aleatório
+      if itens.present?
+        itens.values.as_json.each do |item|
+          puts "Id: #{article.id} - Title: #{article.title} - Item: #{item["99"]}"
+        end
+      else
+        puts "Id: #{article.id} - Title: #{article.title} - Item: Nil ou '' ou {} }"
+      end
+    end
+  end
+
 end
