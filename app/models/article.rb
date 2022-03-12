cf44f8412f31fcb60a5dcbbb389cc63496115e72
Fernando Mendes <fernando.msantos1@gmail.com>
2022-03-12 01:25:19 +0000

    Criar lista clonadas de objetos para atributos com valores em hash

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,7 +6,8 @@ class Article < ApplicationRecord
   validates :description, presence: true, length: {maximum: 200}
   
   def include_description2
-    self.description2 = self.description.capitalize  
+    self.description2 = {"123"=> {"0"=> {"99"=> "10"}, "1"=> {"99"=> "20"}, "2"=> {"99"=> "40" } } } 
+    #self.description2 = {"123"=> {"0"=> {"99"=> "10"}, "1"=> {"99"=> "20"} } }  
   end
 
   def virtual_hash
@@ -21,6 +22,8 @@ class Article < ApplicationRecord
     ].sample
   end
 
+
+
   def self.print_itens
     all.each do |article|
       itens = article.virtual_hash # gera uma vez, pois está aleatório
@@ -33,6 +36,26 @@ class Article < ApplicationRecord
         puts "Id: #{article.id} - Title: #{article.title} - Item: Nil ou '' ou {} }"
       end
     end
+  end
+
+  #lista filtrada qualquer = lista
+  #lista clonada por itens = lista.por_itens
+  def self.por_itens
+    b = []
+    all.select(:id).select('description2').each do |a|
+      if a.description2.present? && a.description2.match(/=>/)
+        #valores = JSON.parse(a.description2.gsub("=>", ":"))
+        #valores.values.first.values.each do |i|
+        eval(a.description2).values.first.values.each do |i|
+          c = Marshal.load(Marshal.dump(a))
+          c.description2 = i['99']
+          b << c
+        end
+      else
+        b << a
+      end 
+    end  
+    return b
   end
 
 end
