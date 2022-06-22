b2edf003f435066cd52d8ae6a4eb3ac2853cc5f9
Fernando Mendes <fernando.msantos1@gmail.com>
2022-06-22 14:00:46 +0000

    Add table users

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_222_015_841) do
+ActiveRecord::Schema.define(version: 20_220_622_135_247) do
   create_table 'articles', force: :cascade do |t|
     t.string 'title'
     t.string 'description'
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
     t.text 'description2'
+  end
+
+  create_table 'users', force: :cascade do |t|
+    t.string 'username'
+    t.string 'email'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 end
