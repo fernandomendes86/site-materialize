bba44e466a74fd9d097c1b934d9e10415045f721
Fernando Mendes <fernando.msantos1@gmail.com>
2022-06-30 13:50:24 -0300

    Add gem BCrypt and password to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,6 @@ class User < ApplicationRecord
                     format: { with: URI::MailTo::EMAIL_REGEXP }
 
   before_save { self.email = email.downcase }
+
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_629_012_522) do
+ActiveRecord::Schema.define(version: 20_220_630_150_210) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -30,5 +30,6 @@ ActiveRecord::Schema.define(version: 20_220_629_012_522) do
     t.string 'email'
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
+    t.string 'password_digest'
   end
 end
