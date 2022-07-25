cb390fc5fad07454b8cc35a5878bc6c19be8481e
Fernando Mendes <fernando.msantos1@gmail.com>
2022-07-25 17:32:59 -0300

    List articles at category and edit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,25 @@
 # frozen_string_literal: true
 
 class CategoriesController < ApplicationController
-  before_action :category_params, only: %i[create]
   before_action :require_admin, except: %i[index show]
   before_action :upload_params, only: %i[uploads]
 
   def index
     @categories = Category.order(:name).page(params[:page])
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = 'Category updated successfully'
+      redirect_to @category
+    else
+      render 'edit'
+    end
   end
 
   def new
@@ -26,6 +39,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @articles = @category.articles
   end
 
   def uploads
